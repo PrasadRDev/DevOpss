df8c60e4747c3f2679f8b2b80a8799e22b7383c0
PrasadR287 <prasad.rotti287@gmail.com>
2023-07-27 12:45:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 terraform {
   required_version = ">=1.3.5"
+  # Backend configuration to store tfstate file
+  # Backend is Terraform Cloud organisation "AvayaCloud"
+  # backend "remote" {
+  #   hostname     = "app.terraform.io"
+  #   organization = "AvayaCloud"
+  #   workspaces {
+  #     name = "ecs-gcp-hostinfra-${var.env}"
+  #   }
+  # }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -9,13 +19,11 @@ terraform {
       source  = "hashicorp/google-beta"
       version = ">= 3.5.0, < 5.0.0"
     }
-
     random = {
       source = "hashicorp/random"
     }
   }
 }
-
 
 provider "google" {
   alias = "impersonation"
@@ -23,6 +31,33 @@ provider "google" {
     "https://www.googleapis.com/auth/cloud-platform",
     "https://www.googleapis.com/auth/userinfo.email",
   ]
+}
+
+provider "google" {
+  access_token    = data.google_service_account_access_token.tf_execute_sa.access_token
+  request_timeout = "1800s"
+}
+
+provider "google-beta" {
+  access_token    = data.google_service_account_access_token.tf_execute_sa.access_token
+  request_timeout = "1800s"
+}
+
+# impersonate Terraform Master service account
+# to Obtain temporary access token for Terrafrom Execute Service account
+#
+data "google_service_account_access_token" "tf_execute_sa" {
+  provider               = google.impersonation
+  target_service_account = var.sa_account
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "3600s"
+}
+
+# Set TF Execute Service account as variable
+variable "sa_account" {
+  description = "SA Account"
+  type        = string
+  default     = "root-sa-terraform@mypoc-389310.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "iam" {
